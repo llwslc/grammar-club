61240f5f29af3b1bb4081fc562f4e3aefe434b09
llwslc <llwslc@gmail.com>
2020-05-01 03:28:54 +0800

    contents

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,33 @@ module.exports = {
     displayAllHeaders: true,
     sidebar: 'auto',
     sidebar: {
-      '/content/': [['Preface', '序'], 'Introduction', 'Contents', 'Chapter01', 'Chapter02']
+      '/content/': [
+        ['Preface', '序'],
+        'Introduction',
+        'Contents',
+        'Chapter01',
+        'Chapter02',
+        'Chapter03',
+        'Chapter04',
+        'Chapter05',
+        'Chapter06',
+        'Chapter07',
+        'Chapter08',
+        'Chapter09',
+        'Chapter10',
+        'Chapter11',
+        'Chapter12',
+        'Chapter13',
+        'Chapter14',
+        'Chapter15',
+        'Chapter16',
+        'Chapter17',
+        'Chapter18',
+        'Chapter19',
+        'Chapter20',
+        'Chapter21',
+        'Chapter22'
+      ]
     },
     nav: [{ text: '目录', link: '/content/Preface' }]
   }
