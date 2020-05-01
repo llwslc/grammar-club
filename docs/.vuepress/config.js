75b7d8155884dd4e59a4d5669f35c98d6aad9fbb
llwslc <llwslc@gmail.com>
2020-05-01 23:57:37 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,34 +5,40 @@ module.exports = {
   themeConfig: {
     repo: 'llwslc/grammar-club',
     displayAllHeaders: true,
-    sidebar: 'auto',
+    smoothScroll: true,
     sidebar: {
       '/content/': [
         ['Preface', '序'],
         'Introduction',
         'Contents',
-        'Chapter01',
-        'Chapter02',
-        'Chapter03',
-        'Chapter04',
-        'Chapter05',
-        'Chapter06',
-        'Chapter07',
-        'Chapter08',
-        'Chapter09',
-        'Chapter10',
-        'Chapter11',
-        'Chapter12',
-        'Chapter13',
-        'Chapter14',
-        'Chapter15',
-        'Chapter16',
-        'Chapter17',
-        'Chapter18',
-        'Chapter19',
-        'Chapter20',
-        'Chapter21',
-        'Chapter22'
+        {
+          title: '第一篇 初级句型--简单句',
+          collapsable: false,
+          children: [
+            'Chapter01',
+            'Chapter02',
+            'Chapter03',
+            'Chapter04',
+            'Chapter05',
+            'Chapter06',
+            'Chapter07',
+            'Chapter08',
+            'Chapter09',
+            'Chapter10',
+            'Chapter11'
+          ]
+        },
+        {
+          title: '第二篇 中级句型--复句',
+          collapsable: false,
+          children: ['Chapter12', 'Chapter13', 'Chapter14', 'Chapter15']
+        },
+        {
+          title: '第三篇 高级句型--简化从句',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: ['Chapter16', 'Chapter17', 'Chapter18', 'Chapter19', 'Chapter20', 'Chapter21', 'Chapter22']
+        }
       ]
     },
     nav: [{ text: '目录', link: '/content/Preface' }]
