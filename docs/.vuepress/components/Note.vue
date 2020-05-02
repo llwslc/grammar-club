15d2fb101ebe89e140c00204a346366a1add1bf9
llwslc <llwslc@gmail.com>
2020-05-02 20:29:32 +0800

    Create Note.vue

---
+++ docs/.vuepress/components/Note.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="note_main">
+    <b class="note_title"><slot /></b>
+    <b>{{ note }}</b>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['note']
+};
+</script>
+
+<style lang="stylus">
+.note_main
+  display: inline-grid;
+  text-align: center;
+  text-indent: 0;
+
+.note_title
+  text-decoration: underline;
+</style>
