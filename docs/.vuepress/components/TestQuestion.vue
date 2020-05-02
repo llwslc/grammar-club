0b8096045c03c5af10a365eaf779d22ecd7c9621
llwslc <llwslc@gmail.com>
2020-05-02 20:29:37 +0800

    Create TestQuestion.vue

---
+++ docs/.vuepress/components/TestQuestion.vue
@@ -0,0 +1,31 @@
+<template>
+  <details>
+    <summary class="test_summary">
+      <div class="test_question">{{ q }}</div>
+    </summary>
+
+    <div class="test_answer"><slot /></div>
+  </details>
+</template>
+
+<script>
+export default {
+  props: ['q']
+};
+</script>
+
+<style lang="stylus">
+.test_summary
+  margin-top: 1em;
+
+.test_question
+  display: inline-block;
+  text-indent: 0;
+  line-height: 1.7;
+  margin-top: 0;
+  margin-bottom: 0;
+
+.test_answer
+  margin-top: 1em;
+  margin-left: 1em;
+</style>
