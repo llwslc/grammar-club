19853d9ab41873d586a630cd2dbc3f715d7ac98c
llwslc <llwslc@gmail.com>
2020-05-08 03:37:38 +0800

    Create Tense.vue

---
+++ docs/.vuepress/components/Tense.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tense_main">
+    <img :src="img" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: String
+  }
+};
+</script>
+
+<style lang="stylus">
+.tense_main
+  text-align: center;
+</style>
