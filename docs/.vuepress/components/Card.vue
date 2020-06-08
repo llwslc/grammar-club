d25be3883f18778fd680f8a610f417af2edb4b9b
llwslc <llwslc@gmail.com>
2020-06-09 02:09:34 +0800

    Create Card.vue

---
+++ docs/.vuepress/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card_main">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus">
+.card_main
+  border: 2px solid #9a9a9a;
+  margin: 0 -1em;
+  padding: 0 1em 0 1em;
+</style>
