d41864e58453e49bb6f9ac8d92dfc74a98de2059
llwslc <llwslc@gmail.com>
2020-06-12 01:50:46 +0800

    Create Quote.vue

---
+++ docs/.vuepress/components/Quote.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="quote_main">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {}
+};
+</script>
+
+<style lang="stylus">
+.quote_main
+  background: #ffe6e6;
+</style>
